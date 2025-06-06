7fa7218f14ecd6e259994c0de7d2fa00abce9df3
unknown <nahomkeneni4@gmail.com>
2025-06-06 01:42:58 -0700

    jwt_file_updated

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,95 +1,103 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/abik1221/Tewanay-Engineering_Intership/database"
+	"github.com/abik1221/Tewanay-Engineering_Intership/helpers"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var userCollection = database.OpenCollection((dataase.Client, "user"))
+var userCollection = database.OpenCollection(database.Client, "user")
 
 func GetUsers() gin.HandlerFunc {
 	return func(c *gin.Context) {
-       ctx, cancel := context.WithTimeout(Context.Background(), 100 * time.Second)
-	   defer cancel()
+		ctx, cancel := context.WithTimeout(Context.Background(), 100*time.Second)
+		defer cancel()
 	}
 }
 
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
-          var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
-		  defer cancel()
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
-		  userId := c.Param("user_id")
+		userId := c.Param("user_id")
 
-		  var user models.User
+		var user models.User
 
-		  err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
 
-		  if err != nil {
+		if err != nil {
 			c.Json(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		  }
+		}
 
-		  c.JSON(http.StatusOK, user)
+		c.JSON(http.StatusOK, user)
 	}
 }
 
 func Signup() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-         var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
-		 defer cancel()
-		 var user models.User
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var user models.User
 
-		 if err := c.BindJSON(&user); err != nil {
+		if err := c.BindJSON(&user); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": err.Error(),
 			})
 			return
-		 }
-      
-		 validation_err := validate.Struct(user)
-		 if validation_err != nil {
+		}
+
+		validation_err := validate.Struct(user)
+		if validation_err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": validation_err.Error(),
 			})
 			return
-		 }
+		}
 
-		 count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
-		 if err != nil {
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
-		 }
-		 if count > 0 {
+		}
+		if count > 0 {
 			c.JSON(http.StatusConflict, gin.H{"error": "Email already exists"})
 			return
-		 }
+		}
 
-		 password := HashPassward(user.Password)
-		 user.Password = &password
+		password := HashPassward(user.Password)
+		user.Password = &password
 
-		 count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
-		 if err != nil {
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
-		 }
-		 if count > 0 {
+		}
+		if count > 0 {
 			c.JSON(http.StatusConflict, gin.H{"error": "Phone number already exists"})
 			return
-		 }
+		}
 
-		 user.CreatedAt = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.CreatedAt,_ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
-		 user.UpdatedAt = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt,_ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
-		 user.ID = primitive.NewObjectID()
-		 user.user_id = user.ID.Hex()
+		user.ID = primitive.NewObjectID()
+		user.user_id = user.ID.Hex()
 
-		token, refrest_tokens, _ :=  helper.GenerateAllTokens(* user.Email, *user.First_Name, *user.Last_Name, user.user_id)
-		 user.Token = &token
-		 user.Refresh_Token = &refrest_tokens
+		token, refrest_tokens, _ := helpers.GenerateAllTokens(*user.Email, *user.First_Name, *user.Last_Name, user.user_id)
+		user.Token = &token
+		user.Refresh_Token = &refrest_tokens
 
-        result, err :=userCollection.InsertOne(ctx, user)
-		 if err != nil {
+		result, err := userCollection.InsertOne(ctx, user)
+		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
@@ -101,7 +109,7 @@ func Signup() gin.HandlerFunc {
 
 func Login() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancel()
 		var Found_user models.User
 		var user models.User
--- helpers/token.go
+++ helpers/token.go
@@ -1,15 +1,65 @@
 package helpers
 
-import()
+import (
+	"os"
+	"time"
 
-func GenerateAllTokens() (){
-    
+	"github.com/abik1221/Tewanay-Engineering_Intership/database"
+	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type signedDetails struct {
+	Email      string
+	First_Name string
+	Last_Name  string
+	User_id    string
+	jwt.StandardClaims
 }
 
-func UpdateAllTokens() () {
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var secretKey = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstname string, lastname string, user_id string) (signedToken string, refresh_token string, err error) {
+
+	claims := &signedDetails{
+		Email:      email,
+		First_Name: firstname,
+		Last_Name:  lastname,
+		User_id:    user_id,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+	refresh_claims := &signedDetails{
+		Email:      email,
+		First_Name: firstname,
+		Last_Name:  lastname,
+		User_id:    user_id,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(72)).Unix(),
+		},
+	}
+
+	tokens, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(secretKey))
+	if err != nil {
+		return "", "", err
+	}
+
+	refresh_tokens, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refresh_claims).SignedString([]byte(secretKey))
+	if err != nil {
+		return "", "", err
+	}
+
+	return tokens, refresh_tokens, nil
 
 }
 
-func ValidateAllTokens() (){
-	
+func UpdateAllTokens() {
+
+}
+
+func ValidateAllTokens() {
+
 }
